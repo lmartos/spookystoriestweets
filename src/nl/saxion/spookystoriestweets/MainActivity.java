0d11512051b27f7569f60cc2992aafc15da4b0a2
Laurens Martos <l-martos@hotmail.com>
2015-05-13 11:42:34 +0200

    json parsing v1

--- src/nl/saxion/spookystoriestweets/MainActivity.java
+++ src/nl/saxion/spookystoriestweets/MainActivity.java
@@ -4,26 +4,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Observable;
+import java.util.Observer;
+
+import nl.saxion.spookystoriestweets.model.Model;
+import nl.saxion.spookystoriestweets.model.Tweet;
+import nl.saxion.spookystoriestweets.view.CustomAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Observer {
+	
+	private CustomAdapter adapter;
+	private ListView tweets;
+	private Model model;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		TweetApplication app = (TweetApplication) getApplicationContext();
+		model = app.getModel();
+		model.addObserver(this);
+		adapter = new CustomAdapter(this.getBaseContext(), R.layout.tweet, model.getData());
+		tweets = (ListView) findViewById(R.id.listView1);
+		tweets.setAdapter(adapter);
 		
 		try {
 			String json = readAssetIntoString("json.txt");
-		} catch (IOException e) {
+			JSONObject jObject = new JSONObject(json);
+			JSONArray jArray = jObject.getJSONArray("statuses");
+			
+			for(int i = 0; i < jArray.length(); i++){
+				JSONObject temp;
+				temp = jArray.getJSONObject(i);
+				Tweet parsedTweet = new Tweet(temp.getString("name") ," ", temp.getString("text"), temp.getString("created_at"), 0 ,0);
+				model.addTweet(parsedTweet);
+
+				
+			}
+			
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 	
 	private String readAssetIntoString(String filename) throws IOException {
@@ -69,5 +104,12 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void update(Observable observable, Object data) {
+		adapter.notifyDataSetChanged();
+		tweets.setBackgroundColor(Color.BLACK);
+		
 	}
 }
--- src/nl/saxion/spookystoriestweets/model/Model.java
+++ src/nl/saxion/spookystoriestweets/model/Model.java
@@ -10,6 +10,16 @@ public class Model extends Observable {
 	private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 	private String filename;
 	
+	public void addTweet(Tweet tweet){
+		tweets.add(tweet);
+		setChanged();
+		notifyObservers();
+	}
+	
+	public ArrayList<Tweet> getData(){
+		return tweets;
+	}
+	
 	
 
 	
